994a4f47911e98a838148bf9041ba64f542e6caf
binf-gau <65479519+Binf-gau@users.noreply.github.com>
2021-11-07 10:13:03 +0700

    'initial'

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '106068d6-4705-4b15-a3c6-e3f3b335e87a';
+export const MODAL_CLOSE_SEC = 2.5;
